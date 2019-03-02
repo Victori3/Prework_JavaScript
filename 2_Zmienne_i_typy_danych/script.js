f8d50d8c3bd92ad3183a425e4ddee9f46a205351
Viktoriia Strilchuk <strilchuk.victoria@gmail.com>
2019-03-02 18:07:07 +0100

    zmienne i typy danych

--- 2_Zmienne_i_typy_danych/script.js
+++ 2_Zmienne_i_typy_danych/script.js
@@ -1,1 +1,38 @@
+// Zadanie 1
+var number = 3;
+console.log(number);
+console.log(typeof number);
+//Ta zmienna jest typu number i przechowuje wartość 3
+
+var string = "To moja ulubiona liczba";
+console.log(string);
+console.log(typeof string);
+//Ta zmienna jest typu string i przechowuje wartość To moja ulubiona liczba
+
+var numberplusstring = "8 marca";
+console.log(numberplusstring);
+console.log(typeof numberplusstring);
+//Ta zmienna jest typu string i przechowuje wartość 8 marca
+
+var boolean = true;
+console.log(boolean);
+console.log(typeof boolean);
+//Ta zmienna jest wartością logiczną i przechowuje wartość true
+
+var foo = null;
+console.log(foo);
+console.log(typeof foo);
+//Ta zmienna jest wartością specjalną i przechowuje wartość null
+
+//Zadanie 2
+var number1 = 15;
+var number2 = 75;
+var result = 0;
+
+console.log(result = number1 + number2); //wynik 90
+
+//Zadanie 3
+console.log(text);
+/* Bląd - text is not defined */
+
 
