92a6a0f2e93bae4323c8a85a81056aaf96d1a830
Viktoriia Strilchuk <strilchuk.victoria@gmail.com>
2019-03-02 17:13:45 +0100

    Pierwszy skrypt

--- 1_Pierwszy_skrypt/script.js
+++ 1_Pierwszy_skrypt/script.js
@@ -1,1 +1,19 @@
+console.log("Twoje imię");
+
+/* Zadanie 1: Po otwarciu index.html w przeglądarce, nic się nie dzieje dopóki nie otworzymy konsoli, w której ten nasz kod został wyświetlony, ale bez polskich znaków */
+
+/*
+Zadanie 2: Po wprowadzeniu do konsoli operacji arytmetycznej:
+ 2 + 3; dostajemy jako rezultat wynik obliczenia - 5
+10 / 4; - mamy wynik obliczenia 2.5
+30 * 2; - wynik obliczenia 60
+*/
+
+
+/*
+Zadanie 3: Po wprowadzeniu do konsoli linii kodu z tekstem:
+ "Hello World"; - mamy wyświetlony teksty w oknie konsoli
+ Hello World; - występuje błąd "Uncaught SyntaxError: Unexpected identifier
+  "Hello World; - występuje błąd "Uncaught SyntaxError: Invalid or unexpected token
+  */
 
