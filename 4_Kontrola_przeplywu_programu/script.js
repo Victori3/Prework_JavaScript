30cc5f855c74f78f6760395439074cee6ab9e765
Viktoriia Strilchuk <strilchuk.victoria@gmail.com>
2019-03-03 17:48:52 +0100

    Kontrola przeplywu programu

--- 4_Kontrola_przeplywu_programu/script.js
+++ 4_Kontrola_przeplywu_programu/script.js
@@ -1,3 +1,57 @@
+//Zadanie 1
+var number1 = 50;
+var number2 = 100;
+
+if (number1 > number2) {
+    console.log("number1");
+} else {
+    console.log("number2");
+}
+
+//Zadanie 2
+var number3 = 23;
+var number4 = 48;
+var number5 = 42;
+
+if (number3 > number4 && number3 > number5) {
+    console.log(number3);
+}
+else if (number4 > number3 && number4 > number5) {
+    console.log(number4);
+}
+else {
+    console.log(number5);
+}
+
+//Zadanie 3
+for(var i=0; i<10; i++) {
+    console.log("Lubię JavaScript");
+}
+
+//Zadanie 4
+var result = 0;   //pętla, która doda do siebie liczby od 1 do 10.
+for(var i=1; i<=10; i++) {
+    result += i;
+    console.log(result);
+}
+
+//Zadanie 5
+var n = 5;
+for (i=0; i<=n; i++) {
+    if(i % 2 == 0) {
+        console.log(i + " - parzysta");
+    }
+    else {
+        console.log(i + " - nieparzysta");
+    }
+}
+
+//Zadanie 6
+for(var i=0; i<4; i++) {
+    for(var j=0; j<8; j++) {
+        console.log("i="+i+", j="+j);
+    }
+}
 
 // Zadanie 7
 
@@ -16,7 +70,7 @@ var size = 5;
 var lineOfStars = "";
 
 for(var i = 0; i < size; i++) {
-    // W każdym kroku muszę zresetować zmienną, ponieważ zaczcynam tworzenie nowej linii
+    // W każdym kroku muszę zresetować zmienną, ponieważ zaczynam tworzenie nowej linii
     lineOfStars = "";
     for(var j = 0; j < size; j++) {
         //Do zmiennej lineOfStars dodaje gwiazdkę
@@ -27,5 +81,17 @@ for(var i = 0; i < size; i++) {
     console.log(" ");
 
     //Wypisuje w konsoli zmienną lineOfStars (jej zawartość - dlatego jest bez cudzysłosów)
+    console.log(lineOfStars);
+}
+
+
+//Zadanie 8
+
+var size = 5;
+
+var lineOfStars = "";
+
+for (var i = 0; i < size; i++) {
+    lineOfStars = lineOfStars + "*";
     console.log(lineOfStars);
 }
